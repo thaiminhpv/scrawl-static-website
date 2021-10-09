3aaf67c49e2e765d14d94f826c463a88f3080a3f
thaiminhpv <thaiminhpv@gmail.com>
2021-10-09 16:23:30 +0700

    enhance full-local scrawl ability

--- scrawl-static-website.js
+++ scrawl-static-website.js
@@ -1,18 +1,21 @@
-const scrape = require('website-scraper'); 
-const websiteUrl = 'https://www.website-name/'; 
- 
-scrape({ 
-    urls: [websiteUrl], 
-    urlFilter: function (url) { 
-        return url.indexOf(websiteUrl) === 0; 
-    }, 
-    recursive: true, 
-    maxDepth: 50, 
-    prettifyUrls: true, 
-    filenameGenerator: 'bySiteStructure', 
-    directory: './node-website' 
-}).then((data) => { 
-    console.log("Entire website succesfully downloaded"); 
-}).catch((err) => { 
-    console.log("An error ocurred", err); 
-}); 
+import scrape from 'website-scraper';
+
+const websiteUrl = 'https://www.website.com';
+const domain = 'website';
+const outputDir = './website';
+
+scrape({
+    urls: [websiteUrl],
+    urlFilter: function (url) {
+        return url.includes(domain)
+    },
+    recursive: true,
+    maxDepth: 50,
+    prettifyUrls: true,
+    filenameGenerator: 'bySiteStructure',
+    directory: outputDir
+}).then((data) => {
+    console.log("Entire website succesfully downloaded");
+}).catch((err) => {
+    console.log("An error ocurred", err);
+});
